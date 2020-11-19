5b9826682c069ce32af0ed557e0925a5f61bd264
Guozhi Tang <tangguozhi53@gmail.com>
2020-11-18 17:36:30 -0800

    Start the React front end part.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import AppNavbar from './components/AppNavbar';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
